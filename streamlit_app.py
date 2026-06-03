0030e1275d02f91db4ed19db10f685e8c9fcad8f
syifaaulia88 <hooncipa8@gmail.com>
2026-06-03 19:14:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,18 +211,20 @@ elif page == "Tentang Kami":
     st.subheader("Selamat Datang di aplikasi ChemSolv!!")
     st.write("""
 
-Dalam kimia fisik dan kimia analisis, memahami serta menghitung kesetimbangan larutan merupakan keterampilan dasar yang sangat penting. Materi kesetimbangan, mulai dari menentukan kelarutan suatu zat hingga memprediksi kapan endapan akan mulai terbentuk, menjadi bagian tak terpisahkan dari praktikum maupun kegiatan analisis di laboratorium. Namun, tidak jarang pelajar atau praktisi merasa kesulitan ketika harus melakukan perhitungan ini secara manual, apalagi saat menghadapi rumus stoikiometri ion yang berbeda-beda serta perhitungan eksponen yang cukup rumit.
-​Sebagai respons terhadap kebutuhan tersebut, kami menghadirkan ChemSolv - Kalkulator Ksp, sebuah aplikasi berbasis web yang dirancang khusus untuk membantu pengguna dalam melakukan berbagai perhitungan kesetimbangan kimia larutan dengan cepat dan akurat. Aplikasi ini mencakup fitur:
+👨‍💻 Tim Pengembang
+Aplikasi ini merupakan hasil Proyek Tugas Website untuk mata kuliah Logika Pemrograman Komputer.
 
-* Perhitungan Kelarutan (s) secara otomatis berdasarkan nilai Ksp yang diketahui.
-* Perhitungan Konstanta Ksp secara instan dari data kelarutan senyawa.
-* Prediksi Pembentukan Endapan dengan membandingkan nilai Quotient Reaksi (Qsp) dan Ksp.
-* Dukungan Multi-Stoikiometri untuk berbagai bentuk rumus senyawa (seperti tipe AB, AB₂, AB₃, hingga A₂B₃).
+👥 Anggota Kelompok:
 
-​Aplikasi ini dikembangkan sebagai bagian dari Tugas Akhir LPK (Laporan Praktikum Kimia) dengan tujuan memberikan kontribusi nyata dalam pembelajaran kimia larutan dan aktivitas laboratorium, khususnya di bidang kesetimbangan kimia. Dengan antarmuka yang sederhana, estetis, dan fungsional, diharapkan ChemSolv dapat menjadi alat bantu yang andal bagi siswa, mahasiswa, guru, analis, maupun siapa saja yang sedang mempelajari atau bekerja dengan kimia larutan.
-​Gunakan aplikasi ini untuk memperkuat pemahaman konsep dan meningkatkan ketelitian dalam perhitungan kimia Anda. 
+🧑‍🔬 Athiyah Amini Azzahra — 2560587  
+👩‍🔬 Ryel Fandralaro  — 2560766 
+🧑‍🔬 Syifa Aulia Farani Pasha   — 2560792  
+👩‍🔬 Zahra Fitria Sukmawan  — 2560809  
+👩‍🔬 Zalika Imani Hamida  — 2560810  
+Kelas: 1D
 
-Selamat menggunakan, dan semoga bermanfaat!
+🎓 Program Studi: Analisis Kimia
+🏛️ Politeknik AKA Bogor
 
     """)
 
