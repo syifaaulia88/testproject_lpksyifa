ac3191bb85d2bed4227c2753b5d02a3c3237d461
syifaaulia88 <hooncipa8@gmail.com>
2026-06-03 17:33:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ st.markdown("""
 if 'page' not in st.session_state: 
     st.session_state['page'] = "Home" 
 with st.sidebar:
-    st.title("Menu")
+    st.title("Menu ChemSolv")
     if st.button("Kalkulator Ksp"): 
         st.session_state['page'] = "Kalkulator Ksp"
         
