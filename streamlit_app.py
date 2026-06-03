9417c758c0d54e75f91993c01e9cdcb2c06ce194
syifaaulia88 <hooncipa8@gmail.com>
2026-06-03 17:38:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,15 +64,12 @@ with st.sidebar:
     st.title("Menu ChemSolv")
     if st.button("Kalkulator Ksp"): 
         st.session_state['page'] = "Kalkulator Ksp"
-        st.title("🧪 ChemSolv: Kalkulator Kelarutan dan Ksp")
         
     if st.button("Prediksi Endapan"):
         st.session_state['page'] = 'Prediksi Endapan'
-        st.title("🧪 ChemSolv: Kalkulator Kelarutan dan Ksp")
-
+        
     if st.button("Tentang Kami"):
         st.session_state['page'] = 'Tentang Kami'
-        st.title("🧪 ChemSolv: Kalkulator Kelarutan dan Ksp")
     page = st.session_state['page'] 
     st.sidebar.divider()
 
@@ -80,6 +77,7 @@ with st.sidebar:
 # FITUR 1: KALKULATOR KELARUTAN & KSP
 # ==========================================
 if page == "Kalkulator Ksp":
+    st.title("🧪 ChemSolv: Kalkulator Kelarutan dan Ksp")
     st.title("🧪 ChemSolv: Kalkulator Kelarutan dan Ksp")
     st.markdown("Hitung **Kelarutan (s)** dari **Ksp**, atau sebaliknya, berdasarkan stoikiometri senyawa.")
     st.divider()
